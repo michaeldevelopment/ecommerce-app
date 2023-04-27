17fe6c45cfeaee18b994c012d14f6df84860a530
Michael Sanabria Martinez <michael.sanabria@globant.com>
2023-04-27 14:26:02 -0500

    added herosection and midsection in home page again

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import HeroSection from "../components/home/HeroSection";
 import MidSection from "../components/home/MidSection";
 
